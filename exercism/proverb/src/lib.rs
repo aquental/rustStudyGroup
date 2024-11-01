809076d0a836d2b6dde917a3aa6c8ba9aef32626
Antonio Quental <antonio.quental@gmail.com>
2024-11-01 10:16:41 -0300

    solution to Proverb

--- exercism/proverb/src/lib.rs
+++ exercism/proverb/src/lib.rs
@@ -1,3 +1,15 @@
+use std::fmt::Write;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    todo!("build a proverb from this list of items: {list:?}")
+    list.windows(2).fold(String::new(), |mut output, items| {
+        let _ = writeln!(
+            output,
+            "For want of a {} the {} was lost.",
+            items[0], items[1]
+        );
+        output
+    }) + &list
+        .first()
+        .map(|all| format!("And all for the want of a {}.", all))
+        .unwrap_or("".to_string())
 }
