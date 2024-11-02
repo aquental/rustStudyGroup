82f3b6db92734b823bdc1cab85cade0c6c26bf74
Antonio Quental <antonio.quental@gmail.com>
2024-11-02 19:51:20 -0300

    solution Kindergarten Garden

--- exercism/kindergarten-garden/src/lib.rs
+++ exercism/kindergarten-garden/src/lib.rs
@@ -1,3 +1,21 @@
+fn plant_name(c: char) -> &'static str {
+    match c {
+        'R' => "radishes",
+        'C' => "clover",
+        'G' => "grass",
+        'V' => "violets",
+        _ => "",
+    }
+}
+
 pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
-    todo!("based on the {diagram}, determine the plants the {student} is responsible for");
+    const CHILDREN: [&str; 12] = [
+        "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph",
+        "Kincaid", "Larry",
+    ];
+    let i = CHILDREN.iter().position(|&n| n == student).unwrap() * 2;
+    diagram
+        .lines()
+        .flat_map(|line| line[i..i + 2].chars().map(plant_name))
+        .collect()
 }
