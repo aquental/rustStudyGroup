b62155d053bc419389b2b0b0d018a3222b4d086a
Antonio Quental <antonio.quental@gmail.com>
2024-11-12 18:46:10 -0300

    final version Sublist

--- exercism/sublist/src/lib.rs
+++ exercism/sublist/src/lib.rs
@@ -7,6 +7,17 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    todo!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    if _first_list == _second_list {
+        Comparison::Equal
+    } else if _second_list.starts_with(_first_list) || _second_list.ends_with(_first_list) {
+        Comparison::Sublist
+    } else if _first_list.starts_with(_second_list) || _first_list.ends_with(_second_list) {
+        Comparison::Superlist
+    } else if _first_list.len() > _second_list.len() {
+        sublist(&_first_list[1..], _second_list)
+    } else if _second_list.len() > _first_list.len() {
+        sublist(_first_list, &_second_list[1..])
+    } else {
+        Comparison::Unequal
+    }
 }
-
