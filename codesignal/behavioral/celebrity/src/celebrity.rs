26079b81f0e54ebc9dffdd122e036e82e9b3d275
aquental <aquental@users.noreply.github.com>
2025-07-06 20:34:12 -0300

    solution

--- codesignal/behavioral/celebrity/src/celebrity.rs
+++ codesignal/behavioral/celebrity/src/celebrity.rs
@@ -6,3 +6,25 @@ use std::collections::HashMap;
 // - Implement the add_follower method in the Celebrity struct to add a follower to the HashMap
 // - Implement the remove_follower method in the Celebrity struct to remove a follower from the HashMap
 // - Implement the post_update method in the Celebrity struct to notify all followers with the update
+pub struct Celebrity {
+    followers: HashMap<String, Box<dyn Follower>>,
+}
+
+impl Celebrity {
+    pub fn new() -> Self {
+        Celebrity {
+            followers: HashMap::new(),
+        }
+    }
+    pub fn add_follower(&mut self, follower_id: String, follower: Box<dyn Follower>) {
+        self.followers.insert(follower_id, follower);
+    }
+    pub fn remove_follower(&mut self, follower_id: &str) {
+        self.followers.remove(follower_id);
+    }
+    pub fn post_update(&self, update: &str) {
+        for follower in self.followers.values() {
+            follower.notify(update);
+        }
+    }
+}
--- codesignal/behavioral/celebrity/src/follower.rs
+++ codesignal/behavioral/celebrity/src/follower.rs
@@ -3,3 +3,24 @@
 
 // TODO: Implement a ConcreteFollower struct that implements the Follower trait
 // - Override the notify method in the ConcreteFollower struct to display the message "<name> received update: <update>"
+pub trait Follower {
+    fn notify(&self, update: &str);
+}
+
+pub struct ConcreteFollower {
+    name: String,
+}
+
+impl ConcreteFollower {
+    pub fn new(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+        }
+    }
+}
+
+impl Follower for ConcreteFollower {
+    fn notify(&self, update: &str) {
+        println!("{} received update: {}", self.name, update);
+    }
+}
